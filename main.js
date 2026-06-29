d80cf70ef6c52e010a552cdbfd4126c1893e5e16
Ana Grilo <metaspheres@gmail.com>
2026-06-29 16:07:02 +0100

    fixed joiningCalculations function

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 let calc_list = []
 let currentNumber = ""
 
-number1 = Number(prompt("Insert the first number:")) // for testing in the console, uncommnent if not needed and we delete at the end of the assignment
-number2 = Number(prompt("Insert the second number:"))
-operator = prompt("insert the operator:")
-
-
 // Basic arithmetic functions
 const add = (a, b) => a + b
 const subtract = (a, b) => a - b
@@ -47,6 +42,18 @@ const operate = (number1, number2, operator) => {
 }
 
 
+function joiningCalculations(calc_list){
+    let result = calc_list[0]
+
+    for(let i = 1; i < calc_list.length; i += 2){
+        let operator = calc_list[i]
+        let number = calc_list[i + 1]
+        result = operate(result, number, operator)
+    }
+    return result 
+}
+
+
 function expect(actual) {
   return {
     toBe: (expected) => {
@@ -58,6 +65,7 @@ function expect(actual) {
     },
   }
 }
+
 
 // --- Unit Tests ---
 console.log("--- Running Unit Tests ---")
