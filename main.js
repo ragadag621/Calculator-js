e03144ba721fc2b8ed6e18ff87535ed4c6068e1a
Ana Grilo <metaspheres@gmail.com>
2026-06-29 14:46:35 +0100

    operators functions added

---
+++ main.js
@@ -0,0 +1,15 @@
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => b === 0 ? " Error!" : a / b;
+
+console.log(add(2, 3))
+
+console.log(subtract(4, 8))
+
+console.log(multiply(4, 8))
+
+console.log(divide(8, 0))
+
+
+
